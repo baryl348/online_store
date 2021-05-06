5dfd447bbc7dc2ea464e850589ccba0976fd61eb
baryl <edotov.egor@gmail.com>
2021-05-06 18:48:16 +0300

    create auth

--- client/src/redux/reducers/auth-reducer.ts
+++ client/src/redux/reducers/auth-reducer.ts
@@ -8,27 +8,56 @@ const initialState = {
     secondName: null as string | null,
     email: null as string | null,
     isAuth: false as boolean,
-    isLoading: false as boolean
+    isLoading: false as boolean,
+    error: null as string | null
 }
 
 
 const authReducer = (state = initialState, action: ActionTypes) => {
     switch (action.type) {
-        case "SET_USER_DATA": { return { ...state, ...action.payload } }
-        case "SET_STATUS_LOADING": { return { ...state, ...action.payload } }
+        case "SET_USER_DATA"
+            && "SET_STATUS_LOADING"
+            && 'SET_ERROR': { return { ...state, ...action.payload } }
+        // case "SET_STATUS_LOADING": { return { ...state, ...action.payload } }
         default:
             return state
     }
 }
 const actions = {
     setAuthUserData: (userId: number, firstName: string, secondName: string, email: string, isAuth: boolean) => ({ type: "SET_USER_DATA", payload: { userId, firstName, secondName, email, isAuth } } as const),
-    setStatusLoading: (isLoading: boolean) => ({ type: "SET_STATUS_LOADING", payload: { isLoading } } as const)
+    setStatusLoading: (isLoading: boolean) => ({ type: "SET_STATUS_LOADING", payload: { isLoading } } as const),
+    setError: (error: string) => ({ type: 'SET_ERROR', payload: { error } } as const)
 }
 
 export const registrationThunk = (firstName: string, secondName: string, email: string, password: string): ThunkType => async (dispatch) => {
-    await auth.registration(firstName, secondName, email, password)
+    try {
+        dispatch(actions.setStatusLoading(true))
+        const registrationData = await auth.registration(firstName, secondName, email, password)
+        const token = registrationData.data.token
+        window.localStorage.setItem('token', token)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        dispatch(actions.setStatusLoading(false))
+    }
 }
-export const loginThunk = (email: string, password: string): ThunkType => async (dispatch) => { }
+export const loginThunk = (email: string, password: string): ThunkType => async (dispatch) => {
+    try {
+        const loginData = await auth.login(email, password)
+        dispatch(actions.setStatusLoading(true))
+        const { firstName, secondName, userId } = loginData.data.login
+        dispatch(actions.setAuthUserData(firstName, secondName, loginData.data.login.email, userId, true))
+        const token = loginData.data.token
+        window.localStorage.setItem('token', token)
+    } catch (error) {
+        console.log()
+    } finally {
+        dispatch(actions.setStatusLoading(false))
+    }
+
+
+
+}
 
 
 export default authReducer
